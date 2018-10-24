ce53a83329b862115a100c48b28c26fd08ece5e7
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-10-23 23:16:48 -0700

    add the colley plot

---
+++ main/main_get_nba_color_palette.R
@@ -0,0 +1,101 @@
+library(tidyverse)
+library(rvest)
+library(rio)
+library(magrittr)
+
+setwd("C:/Users/Wenyao/Desktop/R/Who-is-number-1")
+
+
+#==== read the web page containing the color palette of every NBA team ====
+nba_color_html <- read_html("https://teamcolorcodes.com/nba-team-color-codes/") %>% 
+  html_nodes(".team-button") %>% 
+  head(-1)
+
+
+#==== parse the html css ====
+# parse css into named vectors
+# e.g., "background-color: #E03A3E; color: white; border-bottom: 4px solid #C1D32F;"
+# will become 
+# c("background-color" = "#E03A3E", "color" = "white", border-bottom = "4px solid #C1D32F")
+css_to_vector <- function(css){
+  
+  key_value_pairs <- strsplit(css, ";")[[1]] %>% str_trim()
+  
+  values <- key_value_pairs %>%
+    strsplit(":") %>%
+    map(last) %>% 
+    map(str_trim) %>% 
+    unlist()
+  
+  keys <- key_value_pairs %>% 
+    strsplit(":") %>% 
+    map(first) %>% 
+    map(str_trim) %>% 
+    unlist()
+  
+  values %>% set_names(keys)
+}
+
+# collect major and minor color from css
+nba_team_color_palette <- tibble(
+  team = nba_color_html %>% html_text(),
+  major_color = nba_color_html %>% 
+    map(html_attr, name = "style") %>% 
+    map(css_to_vector) %>% 
+    map(extract2, "background-color") %>% 
+    unlist(),
+  minor_color = nba_color_html %>% 
+    map(html_attr, name = "style") %>% 
+    map(css_to_vector) %>% 
+    map(extract2, "border-bottom") %>% 
+    unlist() %>% 
+    str_sub(start = regexpr("[#]", .))
+) %>% 
+  # fix for the white color code
+  mutate(
+    major_color = if_else(major_color == "#000", "#000000", major_color),
+    minor_color = if_else(minor_color == "#000", "#000000", minor_color)
+  )
+
+
+#==== conver full name to short name ====
+team_name_lookup <- tribble(
+  ~team_full_name, ~team_short_name,
+  "Atlanta Hawks", "ATL",
+  "Boston Celtics", "BOS",
+  "Brooklyn Nets", "BKN",
+  "Charlotte Hornets", "CHA",
+  "Chicago Bulls", "CHI",
+  "Cleveland Cavaliers", "CLE",
+  "Dallas Mavericks", "DAL",
+  "Denver Nuggets", "DEN",
+  "Detroit Pistons", "DET",
+  "Golden State Warriors", "GSW",
+  "Houston Rockets", "HOU",
+  "Indiana Pacers", "IND",
+  "Los Angeles Clippers", "LAC",
+  "Los Angeles Lakers", "LAL",
+  "Memphis Grizzlies", "MEM",
+  "Miami Heat", "MIA",
+  "Milwaukee Bucks", "MIL",
+  "Minnesota Timberwolves", "MIN",
+  "New Orleans Pelicans", "NOP",
+  "New York Knicks", "NYK",
+  "Oklahoma City Thunder", "OKC",
+  "Orlando Magic", "ORL",
+  "Philadelphia 76ers", "PHI",
+  "Phoenix Suns", "PHX",
+  "Portland Trail Blazers", "POR",
+  "Sacramento Kings", "SAC",
+  "San Antonio Spurs", "SAS",
+  "Toronto Raptors", "TOR",
+  "Utah Jazz", "UTA",
+  "Washington Wizards", "WAS"
+)
+
+output <- team_name_lookup %>% 
+  left_join(nba_team_color_palette, by = c("team_full_name" = "team"))
+
+
+#==== output ====
+export(output, file = "data/NBA_Color_Palette.csv")
---
+++ main/main_nba_2018-2019_colley_plot.R
@@ -0,0 +1,118 @@
+library(jsonlite)
+library(zoo)
+library(tidyverse)
+library(magrittr)
+library(rio)
+
+setwd("C:/Users/Wenyao/Desktop/R/Who-is-number-1")
+source("./functions/functions_scrape_nba.R")
+source("./functions/functions_general.R")
+source("./functions/functions_colley's_method.R")
+
+
+#==== 2018 - 2019 Season ====
+regular_season_2018_2019 <- scrape_nba_scoreboard(
+  start_date = "2018-10-16",
+  end_date = Sys.Date() %>% as.character()
+) %>% 
+  # remove future games
+  na.omit()
+
+
+#==== feed the match results incrementaly to the ranking algorithm
+# the first ranking can be produced after the 2nd day
+# when all teams have at least had one game
+as_of_dates <- seq(from = as.Date("2018-10-18"), to = Sys.Date(), by = 1)
+
+# for each training window, find the Colley ranking
+rankings <- as_of_dates %>% 
+  map(function(as_of_date){
+    tibble(
+      rank = 1:30,
+      team = colleys_method(
+        scoreboard = regular_season_2018_2019 %>% 
+          filter(as.Date(date, "%Y%m%d") <= as_of_date)
+      ) %>% 
+        format_ratings()
+    )
+  }) %>% 
+  set_names(as_of_dates) %>% 
+  bind_rows(.id = "as_of_date") %>% 
+  mutate(
+    as_of_date = as.Date(as_of_date),
+    day = as.numeric(as_of_date - as.Date("2018-10-16") + 1)
+  )
+
+# load the NBA color palette
+nba_color_palette <- import("data/NBA_Color_Palette.csv")
+
+plot <- ggplot(data = rankings, aes(x = day, y = rank, group = team)) +
+  geom_line(aes(alpha = 1, color = team), size = 2) +
+  geom_point(aes(alpha = 1, color = team), size = 6) +
+  geom_point(color = "white", size = 2) +
+  scale_x_continuous(
+    breaks = min(rankings$day):max(rankings$day), 
+    minor_breaks = min(rankings$day):max(rankings$day), 
+    expand = c(.05, .05),
+    # labels = paste0("Day ", min(rankings$day):max(rankings$day))
+    labels = as_of_dates %>% format("%b %d, %Y")
+  ) +
+  scale_y_reverse(breaks = 1:30) +
+  # the label background box on the left side
+  geom_tile(
+    data = rankings %>% filter(day == min(rankings$day)), 
+    aes(x = min(rankings$day) - 0.5, y = rank, fill = team, color = team),
+    height = 0.6, 
+    width = 0.6,
+    size = 1.3
+  ) + 
+  # the label on the left side
+  geom_text(
+    data = rankings %>% filter(day == min(day)),
+    aes(label = team, x = min(rankings$day) - 0.5) , 
+    fontface = "bold", 
+    color = "white", 
+    size = 5
+  ) +
+  # the label background box on the right side
+  geom_tile(
+    data = rankings %>% filter(day == max(day)), 
+    aes(x = max(rankings$day) + 0.5, y = rank, fill = team, color = team),
+    height = 0.6,
+    width = 0.6,
+    size = 1.3
+  ) + 
+  # the label on the right side
+  geom_text(
+    data = rankings %>% filter(day == max(day)),
+    aes(label = team, x = max(rankings$day) + 0.5) ,
+    fontface = "bold",
+    color = "white",
+    size = 5
+  ) +
+  scale_fill_manual(
+    values = nba_color_palette$major_color %>% set_names(nba_color_palette$team_short_name)
+  ) +
+  scale_color_manual(
+    values = nba_color_palette$minor_color %>% set_names(nba_color_palette$team_short_name)
+  ) +
+  labs(
+    x = "",
+    y = "",
+    title = "NBA Power Ranking - Colley's Method",
+    subtitle = paste0("Last updated on ", Sys.Date() %>% format("%b %d, %Y"))
+  ) +
+  theme_bw(base_size = 20) +
+  theme(
+    legend.position = "none",
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks = element_blank()
+  )
+
+
+#==== output =====
+png("output/NBA_Ranking.png", width = 850, height = 1600)
+print(plot)
+dev.off()
