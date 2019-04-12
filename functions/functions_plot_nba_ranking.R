62527f0fb63cd604d840518e84eff448a94b4ad4
Wenyao <38875149+shawenyao@users.noreply.github.com>
2019-04-11 20:55:55 -0400

    functionalize nba plot

---
+++ functions/functions_plot_nba_ranking.R
@@ -0,0 +1,119 @@
+#' plot the evolution of nba power ranking
+#' 
+#' @param start_date the first date of the ranking
+#' @param end_date the last date of the ranking
+#' @param scoreboard_full the complete scoreboard data.frame
+#' @param nba_color_palette the color palette data.frame
+#' @param title plot of the titl
+#' 
+#' @return a ggplot object
+#' 
+plot_nba_ranking <- function(
+  start_date,
+  end_date,
+  scoreboard_full,
+  nba_color_palette,
+  title
+){
+  
+  #==== feed the match results incrementaly to the ranking algorithm ====
+  # (the first ranking can be produced as early as the 2nd day
+  # when all teams have at least had one game)
+  # starting from the end of the first week
+  freqeuncy <- 7
+  as_of_dates <- seq(from = start_date, to = end_date, by = freqeuncy)
+  
+  # for each training window, find the Colley ranking
+  rankings <- as_of_dates %>% 
+    map(function(as_of_date){
+      tibble(
+        rank = 1:30,
+        team = colleys_method(
+          scoreboard = scoreboard_full %>% 
+            filter(as.Date(date, "%Y%m%d") <= as_of_date)
+        ) %>% 
+          format_ratings()
+      )
+    }) %>% 
+    set_names(as_of_dates) %>% 
+    bind_rows(.id = "as_of_date") %>% 
+    mutate(
+      as_of_date = as.Date(as_of_date),
+      day = as.numeric(as_of_date - as.Date(min(scoreboard_full$date), "%Y%m%d") + 1)
+    )
+  
+  # auto-adjust the width of team label
+  label_width <- 0.07 * as.numeric(max(as_of_dates) - min(as_of_dates))
+  
+  
+  #==== plot ====
+  plot <- ggplot(data = rankings, aes(x = day, y = rank, group = team)) +
+    geom_line(aes(alpha = 1, color = team), size = 2) +
+    geom_point(aes(alpha = 1, color = team), size = 6) +
+    geom_point(color = "white", size = 2) +
+    scale_x_continuous(
+      breaks = seq(from = min(rankings$day), to = max(rankings$day), by = freqeuncy), 
+      minor_breaks = seq(from = min(rankings$day), to = max(rankings$day), by = freqeuncy), 
+      expand = c(.05, .05),
+      labels = as_of_dates %>% format("%b %d")
+    ) +
+    scale_y_reverse(breaks = 1:30, sec.axis = dup_axis()) +
+    # the label background box on the left side
+    geom_tile(
+      data = rankings %>% filter(day == min(rankings$day)), 
+      aes(x = min(rankings$day) - label_width, y = rank, fill = team, color = team),
+      height = 0.6, 
+      width = label_width,
+      size = 1.3
+    ) + 
+    # the label background box on the right side
+    geom_tile(
+      data = rankings %>% filter(day == max(day)), 
+      aes(x = max(rankings$day) + label_width, y = rank, fill = team, color = team),
+      height = 0.6,
+      width = label_width,
+      size = 1.3
+    ) + 
+    # the label on the left side
+    geom_text(
+      data = rankings %>% filter(day == min(day)),
+      aes(label = team, x = min(rankings$day) - label_width) , 
+      fontface = "bold", 
+      color = "white", 
+      size = 5
+    ) +
+    # the label on the right side
+    geom_text(
+      data = rankings %>% filter(day == max(day)),
+      aes(label = team, x = max(rankings$day) + label_width) ,
+      fontface = "bold",
+      color = "white",
+      size = 5
+    ) +
+    scale_fill_manual(
+      values = nba_color_palette$major_color %>% set_names(nba_color_palette$team_short_name)
+    ) +
+    scale_color_manual(
+      values = nba_color_palette$minor_color %>% set_names(nba_color_palette$team_short_name)
+    ) +
+    labs(
+      title = title,
+      subtitle = paste0("Last updated on ", as_of_dates %>% tail(1) %>% format("%b %d, %Y"))
+    ) +
+    theme_bw(base_size = 20) +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.04),
+      plot.subtitle = element_text(hjust = 0.03),
+      plot.margin = margin(0, 0, 0, 0, "cm"),
+      axis.text.x = element_text(angle = 50),
+      axis.text.y.left = element_text(margin = margin(0, -1.2, 0, 0, "cm")),
+      axis.text.y.right = element_text(margin = margin(0, 0, 0, -1.2, "cm")),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      panel.border = element_blank()
+    )
+  
+}
--- main/main_nba_2018-2019_colley_plot.R
+++ main/main_nba_2018-2019_colley_plot.R
@@ -11,10 +11,11 @@ setwd("C:/Users/Wenyao/Desktop/R/Who-is-number-1")
 source("./functions/functions_scrape_nba.R")
 source("./functions/functions_general.R")
 source("./functions/functions_colley's_method.R")
+source("./functions/functions_plot_nba_ranking.R")
 
 
 #==== 2018 - 2019 Season game results ====
-regular_season_2018_2019 <- scrape_nba_scoreboard(
+nba_2018_2019 <- scrape_nba_scoreboard(
   start_date = "2018-10-16",
   end_date = Sys.Date() %>% as.character()
 ) %>% 
@@ -25,111 +26,18 @@ regular_season_2018_2019 <- scrape_nba_scoreboard(
     !home_team %in% c("USA", "GNS", "WST")
   )
 
-
-#==== feed the match results incrementaly to the ranking algorithm ====
-# (the first ranking can be produced as early as the 2nd day
-# when all teams have at least had one game)
-# starting from the end of the first week
-freqeuncy <- 7
-as_of_dates <- seq(from = as.Date("2018-10-21"), to = Sys.Date(), by = freqeuncy)
-
-# for each training window, find the Colley ranking
-rankings <- as_of_dates %>% 
-  map(function(as_of_date){
-    tibble(
-      rank = 1:30,
-      team = colleys_method(
-        scoreboard = regular_season_2018_2019 %>% 
-          filter(as.Date(date, "%Y%m%d") <= as_of_date)
-      ) %>% 
-        format_ratings()
-    )
-  }) %>% 
-  set_names(as_of_dates) %>% 
-  bind_rows(.id = "as_of_date") %>% 
-  mutate(
-    as_of_date = as.Date(as_of_date),
-    day = as.numeric(as_of_date - as.Date("2018-10-16") + 1)
-  )
-
-# load the NBA color palette
+#==== NBA color palette ====
 nba_color_palette <- import("data/NBA_Color_Palette.csv")
-
-# auto-adjust the width of team label
-label_width <- 0.07 * as.numeric(max(as_of_dates) - min(as_of_dates))
 
 
 #==== plot ====
-plot <- ggplot(data = rankings, aes(x = day, y = rank, group = team)) +
-  geom_line(aes(alpha = 1, color = team), size = 2) +
-  geom_point(aes(alpha = 1, color = team), size = 6) +
-  geom_point(color = "white", size = 2) +
-  scale_x_continuous(
-    breaks = seq(from = min(rankings$day), to = max(rankings$day), by = freqeuncy), 
-    minor_breaks = seq(from = min(rankings$day), to = max(rankings$day), by = freqeuncy), 
-    expand = c(.05, .05),
-    labels = as_of_dates %>% format("%b %d")
-  ) +
-  scale_y_reverse(breaks = 1:30, sec.axis = dup_axis()) +
-  # the label background box on the left side
-  geom_tile(
-    data = rankings %>% filter(day == min(rankings$day)), 
-    aes(x = min(rankings$day) - label_width, y = rank, fill = team, color = team),
-    height = 0.6, 
-    width = label_width,
-    size = 1.3
-  ) + 
-  # the label background box on the right side
-  geom_tile(
-    data = rankings %>% filter(day == max(day)), 
-    aes(x = max(rankings$day) + label_width, y = rank, fill = team, color = team),
-    height = 0.6,
-    width = label_width,
-    size = 1.3
-  ) + 
-  # the label on the left side
-  geom_text(
-    data = rankings %>% filter(day == min(day)),
-    aes(label = team, x = min(rankings$day) - label_width) , 
-    fontface = "bold", 
-    color = "white", 
-    size = 5
-  ) +
-  # the label on the right side
-  geom_text(
-    data = rankings %>% filter(day == max(day)),
-    aes(label = team, x = max(rankings$day) + label_width) ,
-    fontface = "bold",
-    color = "white",
-    size = 5
-  ) +
-  scale_fill_manual(
-    values = nba_color_palette$major_color %>% set_names(nba_color_palette$team_short_name)
-  ) +
-  scale_color_manual(
-    values = nba_color_palette$minor_color %>% set_names(nba_color_palette$team_short_name)
-  ) +
-  labs(
-    title = "NBA Power Ranking - Colley's Method",
-    subtitle = paste0("Last updated on ", as_of_dates %>% tail(1) %>% format("%b %d, %Y"))
-  ) +
-  theme_bw(base_size = 20) +
-  theme(
-    legend.position = "none",
-    plot.title = element_text(hjust = 0.04),
-    plot.subtitle = element_text(hjust = 0.03),
-    plot.margin = margin(0, -0.03, 0, -0.03, "cm"),
-    axis.text.x = element_text(angle = 50),
-    axis.text.y.left = element_text(margin = margin(0, -1.2, 0, 0, "cm")),
-    axis.text.y.right = element_text(margin = margin(0, 0, 0, -1.2, "cm")),
-    axis.ticks = element_blank(),
-    axis.title = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.border = element_blank()
-  )
-
-print(plot)
+plot_2018_2019 <- plot_nba_ranking(
+  start_date = as.Date("2018-10-21"),
+  end_date = as.Date("2019-04-14"),
+  scoreboard_full = nba_2018_2019,
+  nba_color_palette = nba_color_palette,
+  title = "NBA Power Ranking - Colley's Method"
+)
 
 
 #==== output =====
