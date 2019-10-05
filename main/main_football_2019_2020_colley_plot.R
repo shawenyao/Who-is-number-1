7260cd8ecaa596e0fca0040cd9637b6bbf1dde0c
Wenyao <38875149+shawenyao@users.noreply.github.com>
2019-10-05 14:04:15 -0400

    readability update

--- main/main_football_2019_2020_colley_plot.R
+++ main/main_football_2019_2020_colley_plot.R
@@ -13,7 +13,7 @@ set.seed(350)
 #==== general setup ====
 setwd("C:/Users/Wenyao/Desktop/R/Who-is-number-1")
 
-source("functions/functions_scrape_football.R")
+source("functions/functions_scrape_bbc.R")
 source("functions/functions_general.R")
 source("functions/functions_colley's_method.R")
 source("functions/functions_plot_football_ranking.R")
