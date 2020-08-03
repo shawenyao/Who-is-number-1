20caf612d4452490c9d368d93da92c90122c4d97
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-08-03 01:28:29 -0400

    update R directory

--- main/main_football_2019_2020_colley_plot.bat
+++ main/main_football_2019_2020_colley_plot.bat
@@ -1,4 +1,4 @@
-set rscript="C:\Program Files\R\R-3.5.0\bin\Rscript.exe"
+set rscript="C:\Program Files\R\R-4.0.2\bin\Rscript.exe"
 %rscript% main_football_2019_2020_colley_plot.R
 rundll32 user32.dll,MessageBeep
 pause
